1148bb4929a58bf067a4bf223a6c2a6096619306
malli1011 <mallikarjun.bandi@gmail.com>
2022-12-12 20:25:27 +0000

    DataFrames Basics

---
+++ src/main/scala/dataframes/ColumnsAndExpressions.scala
@@ -0,0 +1,39 @@
+package dataframes
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, column, expr}
+
+object ColumnsAndExpressions extends App {
+
+  val spark = SparkSession.builder()
+    .appName("DF Columns and Expressions")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val carsDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/cars.json")
+
+  // Columns
+  val firstColumn = carsDF.col("Name")
+
+  // selecting (projecting)
+  val carNamesDF = carsDF.select(firstColumn)
+
+  // various select methods
+
+  import spark.implicits._
+
+  carsDF.select(
+    carsDF.col("Name"),
+    col("Acceleration"),
+    column("Weight_in_lbs"),
+    'Year, // Scala Symbol, auto-converted to column
+    $"Horsepower", // fancier interpolated string, returns a Column object
+    expr("Origin") // EXPRESSION
+  )
+
+  // select with plain column names
+  carsDF.select("Name", "Year")
+
+}
